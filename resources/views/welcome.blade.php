9753259d20f2316e42bad993889283e03b82f368
a.borovinskiy <a.borovinskiy@fterra.ru>
2025-04-15 21:44:07 +0700

    first

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel + Vue 3</title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    TEST123
+</div>
+<script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
